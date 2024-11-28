d4d3daa0c341f8c2a44d79526dc0afa797103130
Mathis L'Hôtellier <mathislhotellier@gmail.com>
2024-11-28 15:55:40 +0400

    cleaning code

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
 
 const authMiddleware = async (req, res, next) => {
   const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
